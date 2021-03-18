6ff863a5fa86c66c6e1cb2ba90cfabbd0a590c67
GerdHirsch <kontakt@gerdhirsch.de>
2021-03-18 13:28:51 +0100

    out commented removed

--- SimplePeriodicTimerCUTE/src/Test.cpp
+++ SimplePeriodicTimerCUTE/src/Test.cpp
@@ -9,17 +9,10 @@
 #include "xml_listener.h"
 #include "cute_runner.h"
 
-
-void thisIsATest() {
-	ASSERTM("start writing tests", false);	
-}
-
 void runAllTests(int argc, char const *argv[]) {
 
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> listener(xmlfile.out);
-//	auto runner = cute::makeRunner(lis, argc, argv);
-//	bool success = runner(s, "AllTests");
 
 	cute::makeRunner(listener,argc,argv)(ReceiversTest::make_suite(), "Receivers");
 	cute::makeRunner(listener,argc,argv)(PeriodicTimerTest::make_suite(), "PeriodicTimer");
